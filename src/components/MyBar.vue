3c959ec30a0649a571b22e70c012335ec5d8a05b
alexx sub <alexei.s@list.ru>
2021-05-18 10:59:26 +0300

    dashboard

---
+++ src/components/MyBar.vue
@@ -0,0 +1,49 @@
+<template>
+
+<div class="bar-chart" :style="barStyle">
+    <div class="bar-chart--parent" :style="barStyle1">
+        <div class="bar-chart--parent" :style="barStyle2"/>
+    </div>
+</div>
+
+</template>
+<script>
+export default {
+  name: 'MyBar',
+  data () {
+    return {
+    }
+  },
+  props: {
+    value: {
+      type: Array,
+      default: () => [100, 0]
+    },
+    heignt: {
+      type: Number,
+      default: 5
+    }
+  },
+  computed: {
+    barStyle () {
+      return `height: ${this.heignt}px;background: green;`
+    },
+    barStyle1 () {
+      return `background: rgb(0,255,0,.7);width:${this.value[1] + this.value[0]}%`
+    },
+    barStyle2 () {
+      return `background: red;width:${this.value[1]}%`
+    }
+  }
+}
+</script>
+<style>
+.bar-chart {
+  border-radius:2px;
+}
+.bar-chart .bar-chart--parent {
+  height: 100%;
+  float:right;
+  border-radius:0 2px 2px 0;
+}
+</style>
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -2,16 +2,28 @@
   <q-page>
  <div class="q-pa-md">
     <q-list bordered class="rounded-borders">
-      <q-expansion-item
-        expand-separator
-        icon="fas fa-gem"
-        label="Assets"
-      >
+      <q-expansion-item>
+       <template v-slot:header>
+          <q-item-section avatar>
+            <q-avatar icon="fas fa-gem" color="primary" text-color="white" />
+          </q-item-section>
+
+          <q-item-section>
+            Assets
+          </q-item-section>
+           <q-item-section>
+             <my-bar heignt="10" :value="total"/>
+          </q-item-section>
+
+          <q-item-section side>
+           <div>500 730 055 000  <i class="fas fa-ruble-sign"></i></div>
+          </q-item-section>
+        </template>
   <div class="row q-col-gutter-sm q-ma-xs q-mr-sm">
       <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
         <q-card>
           <q-card-section :class="$q.dark.isActive?'blue_dark':'bg-blue-8'" class="text-white">
-               <div class="row">
+              <div class="row">
               <div class="col-9"/>
               <div class="col-2">
                 <q-icon class="my-icon" size="50px" name="fas fa-server"/>
@@ -28,6 +40,13 @@
                 <div class="col-8">1 730 055 000  <i class="fas fa-ruble-sign"></i></div>
                 <div class="col-2 text-h5 text-right">1264</div>
                <div class="col-1"/>
+            </div>
+            <div class="row">
+              <div class="col-1"/>
+              <div class="col-7">
+                <my-bar :value="total"/>
+              </div>
+              <div class="col-2"/>
             </div>
             </div>
           </q-card-section>
@@ -74,8 +93,8 @@
               <div class="col-11">
                 <div class="text-h6">Front Office</div>
               </div>
-                <div class="col-9"/>
-               <div class="col-2 text-h5 text-right">160</div>
+                <div class="col-7"/>
+               <div class="col-4 text-h5 text-right">160</div>
                <div class="col-1"/>
             </div>
             </div>
@@ -98,8 +117,8 @@
               <div class="col-11">
                 <div class="text-h6">Network</div>
               </div>
-                <div class="col-9"/>
-               <div class="col-2 text-h5 text-right">100 000</div>
+                <div class="col-7"/>
+               <div class="col-4 text-h5 text-right">100 000</div>
                <div class="col-1"/>
             </div>
             </div>
@@ -236,11 +255,13 @@
 </template>
 
 <script>
+import MyBar from 'components/MyBar.vue'
 export default {
   name: 'Dashboard',
+  components: { MyBar },
   data () {
     return {
-
+      total: [30, 5]
     }
   }
 }
@@ -250,56 +271,6 @@ export default {
     opacity: 0.2
 .my-card
     width: 100%
-.bar-chart {
-  background: #f33;
-  height: 1rem;
-  margin-bottom: 2rem;
-}
-.bar-chart .bar-chart--inner
-  height: 100%;
-  display: block;
-  margin-bottom: .3rem;
-  position: relative;
-  will-change: width;
-width:60%;
-
-.bar-chart .bar-chart--inner:after
-  position: absolute;
-  top: 0;
-  right: 0;
-  content: attr(data-unused);
-  width: 20%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.3);
-
-.bar-chart.primary .bar-chart--inner
-  background: green;
-
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-
-.bar-chart .bar-chart--inner {
-	background: #333;
-	height: 100%;
-	display: block;
-	margin-bottom: .3rem;
-	position: relative;
-	will-change: width;
-}
-.bar-chart .bar-chart--inner:after {
-	position: absolute;
-	top: 0;
-	right: 0;
-	content: " ";
-	width: 1rem;
-	height: 100%;
-	background: rgba(0, 0, 0, 0.1);
-}
-
-.bar-chart.primary .bar-chart--inner {
-	background: #e86951;
-}
 
 </style>
 <!-- https://asset.mgmt.tensor.ru/
